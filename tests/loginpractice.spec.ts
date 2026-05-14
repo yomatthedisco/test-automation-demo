eda0b4ee4902e7c7e2d21d68208365033b476a26
Sarhiel <sarhielnacinopa@gmail.com>
2026-05-13 18:49:27 -0800

    test - checkout

--- tests/loginpractice.spec.ts
+++ tests/loginpractice.spec.ts
@@ -717,8 +717,10 @@ test('View All Products', async ({page}: {page:Page}) => {
 
         
         
+    test('Checkout with Empty Last Name', async ({page}: {page:Page}) => {  
 
 
-   
+
+          });
 
 });
